b0cbd186ced9209ac0e991e563669bb8837b9f16
Vikash1804 <vikkyvikash8765s1@gmail.com>
2026-09-04 04:57:12 +0530

    congiure redux store and add user  information

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,29 @@
-
+import { BrowserRouter, Route  , Routes} from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import { Provider } from "react-redux";
+import appstore from "./utils/appStore";
+import Feed from "./components/Feed";
 function App() {
  
 
   return (
     <>
-    <div className="navbar bg-neutral text-neutral-content h-17">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">DevTinder</a>
-  </div>
-  <div className="flex gap-2">
+    <Provider store={appstore}>
+    <BrowserRouter basename="/">
+
+  <Routes>
+    <Route path="/" element={<Body />}>
+     <Route path="/" element={<Feed />} />
+    <Route path="/login" element={<Login />} />
     
-    <div className="dropdown dropdown-end mx-6">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-        </div>
-      </div>
-      <ul
-        tabIndex={-1}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
-  
-    </>
+
+
+  </Route>    
+  </Routes>
+      </BrowserRouter>
+      </Provider>
+   </>
   )
 }
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import { Navigate, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+import { BASE_URL } from '../utils/constants';
+import { useSelector } from 'react-redux';
+const Login = () => {
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  
+  const handleLogin = async() => {
+     try{
+      const res = await axios.post(BASE_URL + "/login",{
+        emailId,
+        password
+      } , {
+        withCredentials: true,
+      })
+
+       dispatch(addUser(res.data))
+      navigate("/");
+      }
+     
+
+      catch(err){
+        console.log(err)  
+     }
+  }
+  const [emailId, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <div className="flex justify-center items-center mt-25">
+   <div className="card card-border bg-base-300 w-96 ">
+  <div className="card-body">
+    <h2 className="card-titlem flex justify-center text-3xl font-bold">Login</h2>
+    <div className="form-control py-2">
+      <label className="label">
+        <span className="label-text text-xl font-bold">Email ID</span>
+      </label>
+      <input type="email"  value = {emailId}
+      placeholder="Email" className="input input-bordered" 
+      onChange={(e) => setEmail(e.target.value)} />
+    </div>
+    <div className="form-control  py-2">
+      <label className="label">
+        <span className="label-text text-xl font-bold">Password</span>
+      </label>
+      <input type="password" placeholder="Password" 
+      className="input input-bordered" value={password} 
+      onChange={(e) => setPassword(e.target.value)} />
+    </div>
+    <div className="card-actions justify-center py-2">
+      <button className="btn btn-primary "
+      onClick = {handleLogin}>
+        Login
+      </button>
+    </div>
+  </div>
+</div>
+</div>
+  )
+}
+
+export default Login
